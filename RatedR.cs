3f339e8faf5ae8f7ecfb8b07df9c5b78b92085fd
Taylor Somers <tsomers@yandex.com>
2020-07-12 17:21:38 -0400

    Add branching logic to determine if user is old enough to see the movie

--- RatedR.cs
+++ RatedR.cs
@@ -7,5 +7,14 @@ class RatedR
     Console.WriteLine("How old are you?");
     string stringUserAge = Console.ReadLine();
     int intUserAge = int.Parse(stringUserAge);
+
+    if (intUserAge >= 17)
+    {
+      Console.WriteLine("You can see the movie!");
+    }
+    else
+    {
+      Console.WriteLine("I'm sorry, you are too young to see the movie.");
+    }
   }
 }
