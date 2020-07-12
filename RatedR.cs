c27ef43a63003b46d48ddcb7f8c40117da365d92
Taylor Somers <tsomers@yandex.com>
2020-07-12 17:17:52 -0400

    Add logic to collect user age

--- RatedR.cs
+++ RatedR.cs
@@ -4,6 +4,8 @@ class RatedR
 {
   static void Main()
   {
-    
+    Console.WriteLine("How old are you?");
+    string stringUserAge = Console.ReadLine();
+    int intUserAge = int.Parse(stringUserAge);
   }
 }
